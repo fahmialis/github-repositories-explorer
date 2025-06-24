9d370f72e41dce864970b79f6bb8abd83d7a2297
fahmialis <fahmi@nusantara.technology>
2025-06-24 18:57:51 +0700

    add loop for list

--- src/pages/main/api/endpoints.ts
+++ src/pages/main/api/endpoints.ts
@@ -1,5 +1,6 @@
 import { makeEndpoint, parametersBuilder } from '@zodios/core';
 import { z } from 'zod';
+import { userSchema } from './schema/user';
 
 const getUserList = makeEndpoint({
   alias: 'getUserList',
@@ -7,12 +8,13 @@ const getUserList = makeEndpoint({
   path: 'search/users',
   parameters: parametersBuilder()
     .addQueries({
-      q: z.string().optional(),
+      q: z.string(),
     })
     .build(),
   response: z.object({
     total_count: z.number(),
     incomplete_results: z.boolean(),
+    items: z.array(userSchema),
   }),
 });
 
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,30 +1,45 @@
 import { Input, Collapse } from 'antd';
 import { dashboardApiHooks } from './api';
+import { useState } from 'react';
+import { useDebounce } from 'ahooks';
 
-export default function index() {
+export default function Main() {
   const text = `
   A dog is a type of domesticated animal.
   Known for its loyalty and faithfulness,
   it can be found as a welcome guest in many households across the world.
 `;
 
+  const [searchKey, setSearchKey] = useState('');
+
   const { data } = dashboardApiHooks.useGetUserList({
-    queries: { q: 'john' },
+    queries: { q: useDebounce(searchKey, { wait: 300 }) },
   });
+
+  console.log({ data });
 
   return (
     <div>
-      <Input.Search placeholder="input search text" className="mb-3" />
-
-      <Collapse
-        items={[
-          {
-            key: '1',
-            label: 'This is default size panel header',
-            children: <p>{text}</p>,
-          },
-        ]}
+      <Input.Search
+        placeholder="input search text"
+        className="mb-3"
+        value={searchKey}
+        onChange={(e) => setSearchKey(e?.target?.value)}
       />
+
+      {data?.items.map((user) => (
+        <div key={user.id} className="mb-5">
+          <Collapse
+            items={[
+              {
+                key: user.id,
+                label: user.login,
+                children: <p>{text}</p>,
+              },
+            ]}
+          />
+        </div>
+      ))}
     </div>
   );
 }
